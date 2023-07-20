c2d8fd3a49d2ee9743eda857080501fc4a7d2d93
TMee3 <theopsna@protonmail.com>
2023-07-20 18:20:09 +0200

    s

--- authenticationold/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.admin import register, ModelAdmin
-from .models import User
-
-@register(User)
-class UserAdmin(ModelAdmin):
-    list_display = ('username', 'email', 'is_superuser', 'is_staff', 'is_active')
-    list_filter = ('is_superuser', 'is_active')
